c4df6706d300554f09a6df1df218e65c4c69c7ae
auna <annah.ramones@gmail.com>
2020-07-28 17:45:09 -0700

    [annah] added and implemented post interface/model

---
+++ src/app/posts/post.model.ts
@@ -0,0 +1,10 @@
+// Defines how object looks like but can't be instantiated
+// We can create our own type to force and object to look like this
+// even though we can't actually use an interface to create an object
+
+//export allows the interface to be used outside of this file
+
+export interface Post {
+  title: string;
+  content: string;
+}
