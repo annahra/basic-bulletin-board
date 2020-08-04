06a8a8be74aa7c84e91c207f3d69065e33fcd06b
auna <annah.ramones@gmail.com>
2020-08-04 15:10:46 -0700

    [annah] added ability to 'post' its just console loggging for now that we dont have a server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -15,21 +19,29 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use( '/api/posts', (req, res, next) => {
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
   const posts = [
     {
-      id: "id1",
-      title: 'First server-side post',
-      content: 'This is coming from the server'
+      id: "fadf12421l",
+      title: "First server-side post",
+      content: "This is coming from the server"
     },
     {
-      id: "id2",
-      title: 'Second server-side post',
-      content: 'This is coming from the server!'
+      id: "ksajflaj132",
+      title: "Second server-side post",
+      content: "This is coming from the server!"
     }
   ];
   res.status(200).json({
-    message: 'Posts fetched successfully!',
+    message: "Posts fetched successfully!",
     posts: posts
   });
 });
