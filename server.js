a08cfedee939384fdc83449f182e62549d3bbe8a
auna <annah.ramones@gmail.com>
2020-08-04 13:47:07 -0700

    [annah] added nodemon package for auto restart of server, added server.js file and app.js file including error handlers and port listeners

--- server.js
+++ server.js
@@ -1,9 +1,55 @@
-const http = require('http');
+const app = require("./backend/app");
+const http = require("http");
+const debug = require("debug")("node-angular");
 
-const server = http.createServer((req, res) => {
-  res.end('This is my first response')
-});
+const normalizePort = val => {
+  //makes sure port is valid number
+  var port = parseInt(val, 10);
 
-//3000 is development port, process.env.PORT gets the port that
-//your hosts listens to when you deploy the app
-server.listen(process.env.PORT || 3000);
+  if (isNaN(port)) {
+    //named pipe
+    return val;
+  }
+
+  if (port >= 0 ) {
+    //port number
+    return port;
+  }
+
+  return false;
+};
+
+const onError = error => {
+  //throws appropriate error based on error code
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  switch(error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  //log that we're listening to incoming requests
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
+server.listen(port);
