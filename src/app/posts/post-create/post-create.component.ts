5527894103862d972ca4361df63d90a100e87903
auna <annah.ramones@gmail.com>
2020-08-05 18:24:14 -0700

    [annah] added ability to update/edit posts

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
 import { PostsService } from '../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-post-create',
@@ -11,18 +12,45 @@ import { PostsService } from '../posts.service';
 export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
+  private mode = 'create';
+  private postId: string;
+  post: Post;
+
   // Output is turns postCreated into an event that we can listen to from the outside
   // @Output() postCreated = new EventEmitter<Post>();
-  postCreated = new EventEmitter<Post>();
+  // postCreated = new EventEmitter<Post>();
 
-  constructor(public postsService: PostsService) { }
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    //parameter in url could change once your on the page (could load
+    //different host id). data needs to change once you edit, so you
+    //need to listen to that
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.post = this.postsService.getPost(this.postId);
+
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
   }
 
-  onAddPost(form: NgForm) {
+  onSavePost(form: NgForm) {
     if(form.invalid) {
       return;
+    }
+
+    if(this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
     }
 
     // const post:Post = {
@@ -31,7 +59,7 @@ export class PostCreateComponent implements OnInit {
     //   content: form.value.content
     // };
     // this.postCreated.emit(post);
-    this.postsService.addPost(form.value.title, form.value.content);
+
     form.resetForm();
   }
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -33,6 +33,12 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    //the find method executs on every post in the array
+    //if it returns true, then that's the post it will return
+    return {...this.posts.find(p => p.id === id)};
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content};
     this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
@@ -42,6 +48,11 @@ export class PostsService {
       this.posts.push(post);
       this.postsUpdated.next([...this.posts]);
      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http.put("http://localhost:3000/api/posts/" + id, post).subscribe(response => console.log(response));
   }
 
   deletePost(postId: string) {
